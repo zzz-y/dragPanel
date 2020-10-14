3898be3bb270f52287d15c723749e606187992fa
zzz-y <zhangyue0-0@qq.com>
2020-10-14 10:09:45 +0800

    add 路由调整，创建组件

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import './style/index.less'
+import '@/icons' // icon
+
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,47 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <DragSides
+    :panelData="sideList"
+    width="15%"
+    :props="{id: 'aaa', name: 'bbb'}">
+    <template v-slot:component="slotProps">
+      <component :is="slotProps.componentName"></component>
+    </template>
+  </DragSides>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import DragSides from '@/components/DragSides.vue'
+import ComponentSide from '../components/sider/componentSide'
+import ResourceSide from '../components/sider/resourceSide'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    DragSides,
+    ComponentSide,
+    ResourceSide,
+  },
+  data () {
+    return {
+      sideList: [
+        {
+          aaa: 'resourceTab',
+          bbb: 'resource-side'
+        },
+        {
+          aaa: 'componentTab',
+          bbb: 'component-side'
+        },
+        {
+          aaa: 'componentTab1',
+          bbb: 'component-side'
+        }
+      ],
+    }
+  },
+  methods: {
+
   }
 }
 </script>
