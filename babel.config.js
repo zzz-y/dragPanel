17f8db432797e03d498c16648fc9f70755366995
zzz-y <zhangyue0-0@qq.com>
2020-10-15 18:31:50 +0800

    update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,5 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ],
-  "plugins": [
-    ["@babel/plugin-proposal-class-properties",
-      {
-        "loose": true
-      }
-    ]
   ]
 }
